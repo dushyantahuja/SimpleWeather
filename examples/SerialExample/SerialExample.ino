230f4e43ff44fb5fa7c55a52793b73ae2712567e
Dushyant <dusht.ahuja@gmail.com>
2019-10-06 18:11:26 +0100

    .

--- examples/SerialExample/SerialExample.ino
+++ examples/SerialExample/SerialExample.ino
@@ -2,17 +2,24 @@
 #include "SimpleWeather.h"
 
 
+//************************************
 // Replace with your wifi credentials
-const char * ssid = "********";
-const char * password = "********";
+const char * ssid = "**************";
+const char * password = "*************";
 
 // Get and API Key by registering on
 // https://openweathermap.org/api
+// and https://darksky.net/dev/account
 
-String Key = "********";
 
+String Key = "*************";
+String DKey = "*************";
+
+weatherData w;
 OpenWeather weather(Key, "London,uk");
 OpenWeather forecast(Key, "London,uk",1);
+
+Darksky ds(DKey,51.5074,0.1278);
 
 
 void setup() {
@@ -29,9 +36,8 @@ void setup() {
 }
 
 void loop() {
-  weatherData w;
+  Serial.println("\nOpenWeather Current:");
   weather.updateStatus(&w);
-  Serial.println();
   Serial.println(w.weather);
   Serial.println(w.description);
   Serial.println(w.id);
@@ -40,8 +46,9 @@ void loop() {
   Serial.println(w.max_temp);
   Serial.println(w.humidity);
   Serial.println(w.rain);
+  Serial.println(weather.getResponse().c_str());
 
-  Serial.println("Forecast");
+  Serial.println("\nOpenWeather Forecast:");
   forecast.updateStatus(&w);
   Serial.println(w.weather);
   Serial.println(w.description);
@@ -51,6 +58,20 @@ void loop() {
   Serial.println(w.max_temp);
   Serial.println(w.humidity);
   Serial.println(w.rain);
+  Serial.println(forecast.getResponse().c_str());
+
+  Serial.println("\nDarksky Current:");
+  ds.updateStatus(&w);
+  Serial.println(w.weather);
+  Serial.println(w.description);
+  Serial.println(w.id);
+  Serial.println(w.current_Temp);
+  Serial.println(w.min_temp);
+  Serial.println(w.max_temp);
+  Serial.println(w.humidity);
+  Serial.println(w.rain);
+  Serial.println(ds.getResponse().c_str());
+
   delay(600000);       // Wait for 600 seconds
   // put your main code here, to run repeatedly:
 }
--- src/SimpleWeather.cpp
+++ src/SimpleWeather.cpp
@@ -6,30 +6,37 @@
 
 //const char fingerprint[] PROGMEM = "AC 06 70 3C 86 04 60 22 06 BE E5 11 A5 37 DB 7D 86 92 4E 1C"; // fingerprint
 
-const size_t capacity = 2*JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(2) + 6*JSON_OBJECT_SIZE(1) + 3*JSON_OBJECT_SIZE(2) + 2*JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 5*JSON_OBJECT_SIZE(8) + 570;
 const char *openweather = "api.openweathermap.org";
 const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
 WiFiClientSecure httpsClient;
 
 OpenWeather::OpenWeather(String Key, String City){
-  _Key = Key;
-  _City = City;
+  //_Key = Key;
+  //_City = City;
   _forecast = false;
-  _url = "/data/2.5/weather?q=" + _City + "&appid=" + _Key +"&units=metric";
+  _url = "/data/2.5/weather?q=" + City + "&appid=" + Key +"&units=metric";
+}
+
+OpenWeather::OpenWeather(String Key, float lat, float longi){
+  //_Key = Key;
+  //_City = City;
+  _forecast = false;
+  _url = "/data/2.5/weather?lat=" + String(lat) + "&long=" + String(longi) + "&appid=" + Key +"&units=metric";
 }
 
 OpenWeather::OpenWeather(String Key, String City, boolean forecast){
-  _Key = Key;
-  _City = City;
+  //_Key = Key;
+  //_City = City;
   _forecast = forecast;
   if(!forecast)
-    _url = "/data/2.5/weather?q=" + _City + "&appid=" + _Key +"&units=metric&cnt=1";
+    _url = "/data/2.5/weather?q=" + City + "&appid=" + Key +"&units=metric&cnt=1";
   else
-    _url = "/data/2.5/forecast?q=" + _City + "&appid=" + _Key +"&units=metric&cnt=2";
+    _url = "/data/2.5/forecast?q=" + City + "&appid=" + Key +"&units=metric&cnt=2";
 }
 
 void OpenWeather::updateStatus(weatherData *w){
   //httpsClient.setFingerprint(fingerprint);
+  const size_t capacity = 2*JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(2) + 6*JSON_OBJECT_SIZE(1) + 3*JSON_OBJECT_SIZE(2) + 2*JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 5*JSON_OBJECT_SIZE(8) + 570;
   httpsClient.setInsecure();
   httpsClient.setTimeout(15000); // 15 Seconds
   delay(1000);
@@ -94,58 +101,78 @@ void OpenWeather::updateStatus(weatherData *w){
   }
 }
 
-
-// Replace with your wifi credentials
-const char * ssid = "DUSHYANT";
-const char * password = "ahuja987";
-
-// Get and API Key by registering on
-// https://openweathermap.org/api
-
-
-
-String Key = "15121d64b58b9086439fed7f00050c04";
-
-weatherData w;
-OpenWeather weather(Key, "London,uk");
-OpenWeather forecast(Key, "London,uk",1);
-
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-      Serial.println("WiFi Failed");
-      while(1) {
-          delay(1000);
-      }
-  }
+String OpenWeather::getResponse(){
+  return _Response;
 }
 
-void loop() {
-  weather.updateStatus(&w);
-  Serial.println();
-  Serial.println(w.weather);
-  Serial.println(w.description);
-  Serial.println(w.id);
-  Serial.println(w.current_Temp);
-  Serial.println(w.min_temp);
-  Serial.println(w.max_temp);
-  Serial.println(w.humidity);
-  Serial.println(w.rain);
+// DarkSky API
 
-  Serial.println("Forecast");
-  forecast.updateStatus(&w);
-  Serial.println(w.weather);
-  Serial.println(w.description);
-  Serial.println(w.id);
-  Serial.println(w.current_Temp);
-  Serial.println(w.min_temp);
-  Serial.println(w.max_temp);
-  Serial.println(w.humidity);
-  Serial.println(w.rain);
-  delay(600000);       // Wait for 600 seconds
-  // put your main code here, to run repeatedly:
+const char *darksky = "api.darksky.net";
+
+
+Darksky::Darksky(String Key, float lat, float longi){
+  _url = "/forecast/" + Key +"/" + String(lat) + "," + String(longi) +"?exclude=minutely,hourly,daily,alerts,flags&units=si";
+}
+
+Darksky(String Key, float lat, float longi, unsigned long timestamp){
+  _url = "/forecast/" + Key +"/" + String(lat) + "," + String(longi) +"," + String(timestamp) + "?exclude=minutely,hourly,daily,alerts,flags&units=si";
+}
+
+
+void Darksky::updateStatus(weatherData *w){
+  //httpsClient.setFingerprint(fingerprint);
+  httpsClient.setInsecure();
+  httpsClient.setTimeout(15000); // 15 Seconds
+  delay(1000);
+
+  //Serial.print("HTTPS Connecting");
+  int r=0; //retry counter
+  while((!httpsClient.connect(openweather, httpsPort)) && (r < 30)){
+      delay(100);
+      Serial.print(".");
+      r++;
+  }
+
+  Serial.print("requesting URL: ");
+  Serial.println(darksky+_url);
+
+  httpsClient.print(String("GET ") + _url + " HTTP/1.1\r\n" +
+               "Host: " + darksky + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+
+  while (httpsClient.connected()) {
+    String line = httpsClient.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      Serial.println(line);
+      break;
+    }
+  }
+
+  Serial.println("reply was:");
+  Serial.println("==========");
+  //httpsClient.readStringUntil('\n'); // The API sends an extra line with just a number. This breaks the JSON parsing, hence an extra read
+  while(httpsClient.connected()){
+    _Response = httpsClient.readString();
+    Serial.println(_Response); //Print response
+  }
+  const size_t capacity = JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(19) + 350;
+  DynamicJsonDocument doc(capacity);
+  deserializeJson(doc,_Response);
+
+    w->description = doc["currently"]["summary"].as<String>();
+    w->weather = doc["currently"]["icon"].as<String>();
+    w->id = 0; //doc["currently"].as<int>();
+    w->current_Temp = doc["currently"]["apparentTemperature"].as<float>(); //Use Apparent Temperature
+    w->min_temp = 0; //doc["currently"].as<float>();
+    w->max_temp = 0; //doc["currently"].as<float>();
+    w->humidity = doc["currently"]["humidity"].as<float>();
+    w->rain = doc["currently"]["precipProbability"].as<float>();
+
+}
+
+String Darksky::getResponse(){
+  return _Response;
 }
